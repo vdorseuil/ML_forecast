a9b2f262427d465b960ba2e88495486ec0f6aa59
valentin.dorseuil@gmail.com <valentin.dorseuil@gmail.com>
2025-02-13 12:49:53 +0100

    Results + baseline + eval

--- eval.py
+++ eval.py
@@ -6,16 +6,25 @@ from sklearn.metrics import mean_squared_error
 
 
 def MSE(y_pred, y_true):
-    return mean_squared_error(y_true, y_pred)
+    "Computes the MSE and the std of the squared errors"
+    squared_errors = (np.array(y_true) - np.array(y_pred)) ** 2
+    plt.hist(squared_errors, bins=1000, label="MSE")
+    plt.xscale('log')
+    return squared_errors.mean(), squared_errors.std()
+
 
 def custom_metric(y_pred, y_true, a=36.97):
-    "Return the average loss in € for Germany"
+    "Return the average loss in € for Germany, mean and std"
     # a = df_results[(df_results["GERMANY_IMPORT(-)_EXPORT(+)_[MW]"]>0) & (df_results["PRODUCT"] == "POS_00_04")]["GERMANY_IMPORT(-)_EXPORT(+)_[MW]"].mean()
     # C = a*avg_cap_price --> you lost all
     y_t = np.array(y_true)
     y_p= np.array(y_pred)
-    loss = (a*np.maximum(y_t-y_p, 0) + a*y_p*(y_t-y_p<0)).mean()*4. #*4 because it lasts 4hours.
-    return loss
+    loss = (a*np.maximum(y_t-y_p, 0) + a*y_p*(y_t-y_p<0))*4. #*4 because it lasts 4hours.
+    plt.hist(loss, bins=1000, label="Custom")
+    plt.xscale('log')
+    plt.legend()
+    plt.savefig("losses_hist.png")
+    return loss.mean(), loss.std()
 
 def S(x, k, p):
     return 1 / (1 + np.exp(-k * (x - p)))
